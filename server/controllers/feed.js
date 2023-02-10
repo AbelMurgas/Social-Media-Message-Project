48f0dd184128656bac324b4b4fc20a76da78b350
Abel Murgas <abelmurgasdev@gmail.com>
2023-02-10 16:17:12 -0500

    update feed controllers

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,52 +1,94 @@
 import validator from "express-validator";
+
+import Post from "../models/post.js";
+
 const { validationResult } = validator;
 
-const getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        creator: {
-          name: "Abel Murgas",
-        },
-        createdAt: new Date(),
-        content: "This is the first post!",
-        imageUrl: "images/Yellow_Duck.jpg",
-      },
-    ],
-  });
+export const getPosts = (req, res, next) => {
+  Post.find()
+    .select("-__v")
+    .then((result) => {
+      if (!result) {
+        error = new Error("Posts no found");
+        error.statusCode = 204;
+        throw error;
+      }
+      res.status(200).json({
+        posts: result,
+      });
+    })
+    .catch((error) => {
+      if (!error.status) {
+        error.status = 500;
+      }
+      next(error);
+    });
 };
 
-const createPost = (req, res, next) => {
+export const createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array())
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, enterred data is incorrect",
-        errors: errors.array(),
-      });
+    const error = new Error("Validation failed, enterred data is incorrect");
+    error.statusCode = 422;
+    throw error;
   }
+  console.log(req.file)
+  if (!req.file) {
+    const error = new Error("No image provided.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\", "/");
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Abel" },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: { name: "Abel Murgas" },
   });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: {
+          _id: new Date().toISOString(),
+          title: title,
+          content: content,
+          creator: { name: "Abel" },
+          createdAt: new Date(),
+        },
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+        
+      }
+      console.log(err)
+      next(err);
+    });
 };
 
-const feedController = {
-  createPost: createPost,
-  getPosts: getPosts,
+export const getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .select("-__v")
+    .then((result) => {
+      if (!result) {
+        const error = new Error("Post not found");
+        error.statusCode = 204;
+        throw error;
+      }
+      res.status(200).json({
+        post: result,
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
 };
-
-export default feedController;
