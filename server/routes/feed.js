7094774af62a5a4aa0575e052f782a5d6f65d973
Abel Murgas <abelmurgasdev@gmail.com>
2023-02-10 16:17:31 -0500

    add controller to the feed route

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,15 +1,17 @@
 import express from "express";
 
-import feedController from "../controllers/feed.js";
+import { getPosts, createPost, getPost } from "../controllers/feed.js";
 
 import feedValidator from "../utils/validator.js";
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get("/posts", getPosts);
 
 // POST /feed/post
-router.post('/post', feedValidator,feedController.createPost);
+router.post("/post", feedValidator, createPost);
+
+router.get("/post/:postId", getPost);
 
 export default router;
