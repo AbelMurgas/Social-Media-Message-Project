0dc0a707ed8034ff4a74282945841ce5527639ce
Abel Murgas <abelmurgasdev@gmail.com>
2023-02-26 21:45:00 -0500

    Added auth middleware

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -2,20 +2,21 @@ import express from "express";
 
 import { getPosts, createPost, getPost, updatePost, deletePost } from "../controllers/feed.js";
 
-import feedValidator from "../utils/validator.js";
+import { feedValidator } from "../utils/validator.js";
+import isAuth from "../middleware/is-auth.js";
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get("/posts", getPosts);
+router.get("/posts",isAuth, getPosts);
 
 // POST /feed/post
-router.post("/post", feedValidator, createPost);
+router.post("/post",isAuth, feedValidator, createPost);
 
-router.get("/post/:postId", getPost);
+router.get("/post/:postId",isAuth, getPost);
 
-router.put("/post/:postId", feedValidator, updatePost);
+router.put("/post/:postId",isAuth, feedValidator, updatePost);
 
-router.delete("/post/:postId", deletePost)
+router.delete("/post/:postId",isAuth, deletePost)
 
 export default router;
